4de9286ec035f5a5154bafb10e6dd4238843040e
NoNmae <arromanyuk.noname@gmail.com>
2026-01-18 15:47:41 +0200

    fix

--- Sources/SDKTest_CAS/Extentions.swift
+++ Sources/SDKTest_CAS/Extentions.swift
@@ -1,31 +1,32 @@
+import SwiftUI
+import Foundation
+import UIKit
 
-//import UIKit
-//
-//public extension UIApplication {
-//    var currentRootViewController: UIViewController? {
-//        connectedScenes
-//            .filter { $0.activationState == .foregroundActive }
-//            .compactMap { $0 as? UIWindowScene }
-//            .first?.windows
-//            .filter { $0.isKeyWindow }
-//            .first?.rootViewController
-//            .compactMap { $0 }
-//            .first
-//    }
-//}
-//
-//// Helper to find top controller if needed (often standard in SDKs)
-//public extension UIViewController {
-//    var topMostViewController: UIViewController {
-//        if let presented = presentedViewController {
-//            return presented.topMostViewController
-//        }
-//        if let nav = self as? UINavigationController {
-//            return nav.visibleViewController?.topMostViewController ?? nav
-//        }
-//        if let tab = self as? UITabBarController {
-//            return tab.selectedViewController?.topMostViewController ?? tab
-//        }
-//        return self
-//    }
-//}
+public extension UIApplication {
+    var currentRootViewController: UIViewController? {
+        connectedScenes
+            .filter { $0.activationState == .foregroundActive }
+            .compactMap { $0 as? UIWindowScene }
+            .first?.windows
+            .filter { $0.isKeyWindow }
+            .first?.rootViewController
+            .compactMap { $0 }
+            .first
+    }
+}
+
+// Helper to find top controller if needed (often standard in SDKs)
+public extension UIViewController {
+    var topMostViewController: UIViewController {
+        if let presented = presentedViewController {
+            return presented.topMostViewController
+        }
+        if let nav = self as? UINavigationController {
+            return nav.visibleViewController?.topMostViewController ?? nav
+        }
+        if let tab = self as? UITabBarController {
+            return tab.selectedViewController?.topMostViewController ?? tab
+        }
+        return self
+    }
+}
