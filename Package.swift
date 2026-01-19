accb21dafbea7f57cf1f5163fceab856375eb529
NoNmae <arromanyuk.noname@gmail.com>
2026-01-19 11:53:59 +0200

    fix

--- Package.swift
+++ Package.swift
@@ -26,9 +26,9 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SDKTest_CAS",
-//            dependencies: [
-//                .product(name: "CleverAdsSolutions", package: "CAS-iOS")
-//            ]
+            dependencies: [
+                .product(name: "CleverAdsSolutions", package: "CAS-iOS")
+            ]
         ),
 
     ]
